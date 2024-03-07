c047d00d44e7c2d6a9f028fa1aafaefc88255d49
Yyuichiii <ritikmehradec@gmail.com>
2024-03-07 17:41:08 +0530

    Used Channel Redis

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -4,16 +4,19 @@ class MyConsumer(AsyncConsumer):
 
     async def websocket_connect(self, event):
         print("1")
+        print(event)
+        print(self.channel_layer)
+        print(self.channel_name)
         await self.send({
             "type": "websocket.accept",
         })
 
     async def websocket_receive(self, event):
-        print("2")
+        print("2",self.channel_name)
         print(event)
         await self.send({
             "type": "websocket.send",
-            "text": "Message from the server",
+            "text": f"Message from the server: {event['text']}",
         })
 
     async def websocket_disconnect(self, event):
