857b7ba6db47556e8ed8d5039d058a1bbfd7de78
Yyuichiii <ritikmehradec@gmail.com>
2024-03-09 15:35:44 +0530

    working on the image upload

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -5,11 +5,12 @@ from asgiref.sync import async_to_sync
 from django.contrib.auth.models import User
 from .models import ChatLog
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class MyConsumer(AsyncConsumer):
+class MyConsumer(AsyncWebsocketConsumer):
 
-    async def websocket_connect(self, event):
+    async def connect(self):
         print("-------------1----------------")
         # Retrieve the user name from the scope
         user = self.scope["user"]
@@ -20,30 +21,34 @@ class MyConsumer(AsyncConsumer):
         # Save the user with the channel name in the database
         await self.save_user_to_chat_log(user, channel_name)
 
-        await self.send({
-            "type": "websocket.accept",
-        })
+        await self.accept()
         print("-------------2----------------")
         
 
-    async def websocket_receive(self, event):
+    async def receive(self, text_data=None, bytes_data=None):
         print("-------------3----------------")
         # Parse the received JSON-formatted message
+        # json_message = event.get("text", "")
+        if bytes_data:
+            print(type(bytes_data))
+            await self.send(bytes_data=bytes_data)
+            return
+        message_data = json.loads(text_data)
+        # print(message_data)
         
-        json_message = event.get("text", "")
-        message_data = json.loads(json_message)
-        print(message_data)
-        message_data_send=json.dumps(message_data)
-        print(message_data_send)
 
-        receiver_channel=await self.Get_Channel_name(message_data['user'])
+        receiver_channel=await self.Get_Channel_name(message_data['receive_id'])
+        
         # This has to work after the database thing is sorted
         if receiver_channel is None:
+            print('sf')
+            data = {
+             "text": "is not online"
+                }
             
-            await self.send({
-                'type': 'websocket.send',
-                'text': 'is Not Online'
-                })
+            # Convert the dictionary to a JSON string
+            json_data = json.dumps(data)
+            await self.send(text_data=json_data)
         else:
 
             # Get the channel layer
@@ -52,16 +57,17 @@ class MyConsumer(AsyncConsumer):
             # Send the message asynchronously
             await channel_layer.send(receiver_channel,{
                 "type": "websocket.send_to_receiver",
-                "text": message_data_send,
+                "text": text_data,
             
                     })
         print("-------------4----------------")
        
 
-    async def websocket_disconnect(self, event):
+    async def disconnect(self, close_code):
         user = self.scope["user"]
         await self.delete_user_from_chat_log(user)
         raise StopConsumer()
+        await self.close()
     
 
     @database_sync_to_async
@@ -87,9 +93,7 @@ class MyConsumer(AsyncConsumer):
         
     
     async def websocket_send_to_receiver(self, event):
-        
+        print(event)
         # Send message to WebSocket
-        await self.send({
-        'type':'websocket.send',
-        'text': event['text'],
-        })
+        await self.send(text_data=event['text'])
+       
